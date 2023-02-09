92dcc2a668ae694aca41f213234eaceb9ba3616c
Sabin Neupane <61322712+sabin26@users.noreply.github.com>
2023-02-09 09:44:32 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:win32/win32.dart';
+import 'package:window_manager/window_manager.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(400, 600),
+    backgroundColor: Colors.transparent,
+    titleBarStyle: TitleBarStyle.hidden,
+  );
+
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.setPosition(
+      const Offset(
+        966,
+        168,
+      ), // x,y = desktopScreenSize - appSize (width,height)
+    );
+    const className = 'FLUTTER_RUNNER_WIN32_WINDOW'; // from win32_window.cpp
+    const windowTitle = 'slide_window'; // application's window name
+
+    final classNamePointer = TEXT(className);
+    final windowTitlePointer = TEXT(windowTitle);
+    final hwnd = FindWindow(classNamePointer, windowTitlePointer);
+
+    final animate = AnimateWindow(hwnd, 1000, AW_SLIDE | AW_HOR_NEGATIVE);
+    log('Animation Successful: ${animate == 1}');
+
+    free(classNamePointer);
+    free(windowTitlePointer);
+  });
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Slide Window',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
